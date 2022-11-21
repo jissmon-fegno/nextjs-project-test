6f6cb6c455bd99461bb1193130152f4fa83eb6be
Sebin-fegno <sebin@fegno.com>
2022-11-21 12:33:35 +0530

    First Commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function FirstPost() {
+  return (
+    <div>
+        <h1>First Post</h1>
+        <h2>
+            <Link href={'/'}
+                style={{color: 'blueviolet'}}
+            >
+                Back to Home
+            </Link>
+        </h2>
+    </div>
+  )
+}
