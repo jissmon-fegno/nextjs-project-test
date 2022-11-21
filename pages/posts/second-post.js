e0232a5f48f6a081549cc7c74cf79cf50e471ad3
Sebin-fegno <sebin@fegno.com>
2022-11-21 12:45:38 +0530

    Commit this

--- pages/posts/second-post.js
+++ pages/posts/second-post.js
@@ -1,26 +1,17 @@
-import Link from "next/link";
+import Link from "next/link"
 
-export default function FirstPost() {
+export default function SecondPost() {
   return (
     <div>
-        <h1>First Post</h1>
+        <h1>Second Post</h1>
         <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
-        <h2>
-            <Link href={'/'}
-                style={{color: 'blueviolet'}}
-            >
-                Back to Home
-            </Link>
-        </h2>
         <h3>
-          <Link
-            href={'/posts/second-post'}
-            style={{color: 'blueviolet'}}
-          >
-            Move to Second Post
-          </Link>
+            <Link href={'/posts/first-post'}
+                style={{color: 'blue'}}>
+                    Back to First Post
+            </Link>     
         </h3>
     </div>
   )
